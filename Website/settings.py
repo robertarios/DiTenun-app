a34f2caaafff2b3037521d5c65d593b9d0d33436
robert <ifs21024@students.del.ac.id>
2025-08-26 21:09:42 +0700

    update

--- Website/settings.py
+++ Website/settings.py
@@ -103,8 +103,8 @@ DATABASES = {
         'NAME': 'GenerateMotif',
         'USER': 'root',
         'PASSWORD':'root',  # Sesuaikan dengan password MariaDB Anda
-        'HOST': 'mysql-db',  # atau 'localhost'
-        'PORT':'3309',
+        'HOST': 'localhost',  # atau 'localhost'
+        'PORT':'3306',
 }
 }
 
